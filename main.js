e65a4ff481d55b39087f8503e7e724500da68bd3
doanhha <dhavan4444@gmail.com>
2026-04-25 11:33:20 +0700

    Remove modal HTML structure and related JavaScript functionality

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-let currentModal = null;
 
+{
+    // <div id="modal-1" class="modal-backdrop">
+    //     <div class="modal-container">
+    //         <button id="modal-close" class="modal-close">&times;</button>
+    //         <div class="modal-content">
+    //             <p>
+    //                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, laborum?
+    //             </p>
+    //         </div>
+    //     </div>
+    // </div>
+}
+
+
+// let currentModal = null;
 // $("#open-modal").onclick = function () {
 //     modal.classList.add("show")
 // }
@@ -19,40 +33,40 @@ let currentModal = null;
 //     }
 // })
 
-$$("[data-modal]").forEach((btn) => {
-    btn.onclick = function () {
-        const modal = $(this.dataset.modal);
-        if (modal) {
-            modal.classList.add("show")
-            currentModal = modal
-        } else {
-            console.error(`${this.dataset.modal} does not exit`)
-        }
-    }
-})
+// $$("[data-modal]").forEach((btn) => {
+//     btn.onclick = function () {
+//         const modal = $(this.dataset.modal);
+//         if (modal) {
+//             modal.classList.add("show")
+//             currentModal = modal
+//         } else {
+//             console.error(`${this.dataset.modal} does not exit`)
+//         }
+//     }
+// })
 
-$$(".modal-close").forEach((btn) => {
-    btn.onclick = function () {
-        const modal = this.closest('.modal-backdrop')
-        if (modal) {
-            modal.classList.remove("show")
-            currentModal = null
-        }
-    }
-})
+// $$(".modal-close").forEach((btn) => {
+//     btn.onclick = function () {
+//         const modal = this.closest('.modal-backdrop')
+//         if (modal) {
+//             modal.classList.remove("show")
+//             currentModal = null
+//         }
+//     }
+// })
 
-$$(".modal-backdrop").forEach(modal => {
-    modal.onclick = function (e) {
-        if (e.target === this) {
-            this.classList.remove("show");
-            currentModal = null
-        }
-    }
-})
+// $$(".modal-backdrop").forEach(modal => {
+//     modal.onclick = function (e) {
+//         if (e.target === this) {
+//             this.classList.remove("show");
+//             currentModal = null
+//         }
+//     }
+// })
 
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && currentModal) {
-        currentModal.classList.remove("show");
-        currentModal = null
-    }
-})
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "Escape" && currentModal) {
+//         currentModal.classList.remove("show");
+//         currentModal = null
+//     }
+// })
